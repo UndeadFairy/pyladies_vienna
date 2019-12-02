eff346adf61fcbbef78dc0342d183f1c2ab2c8d6
Lubomir Bucek <lubomir.bucek@eox.at>
2019-12-02 22:21:01 +0100

    fill solutions from second workshop

--- python_fundamentals_2/fundamentals2.py
+++ python_fundamentals_2/fundamentals2.py
@@ -61,3 +61,69 @@ for i in range(4):
     turtle_move_turn()
 Screen().exitonclick()
 """
+"""
+# 2. Using functions penup and pendown you can say turtle to stop and continue drawing. Draw a dashed line.
+from turtle import forward, left, Screen, penup, pendown
+for i in range(30):
+    pendown()
+    forward(10)
+    left(12)
+    penup()
+    forward(10)
+    
+Screen().exitonclick()
+"""
+"""
+# 3. Draw 3 squares, each turned by 20°
+from turtle import forward, left, Screen
+def draw_house(length_of_side):
+    for j in range(4):
+        forward(length_of_side)
+        left(90)
+for i in range(3):
+    draw_house(150)
+    left(20)
+Screen().exitonclick()
+"""
+"""
+# 4. Draw a rainbow
+from turtle import width, pencolor, left, forward, Screen
+colors = ['red', 'orange', 'yellow', 'lime', 'green', 'cyan', 'blue', 'purple']
+left(90)
+width(30)
+for color in colors:
+    pencolor(color)
+    forward(50)
+    left(-180 / len(colors) - 3)
+Screen().exitonclick()
+"""
+"""
+# 1. Write a program to get the smallest number from a list.
+test_list = [25, 13.34, -43, 105]
+# sort() method modifies the list "in place"
+test_list.sort()
+smallest = test_list[0]
+print(smallest, test_list)
+"""
+"""
+# 2. Write a program to sum all the items in a list.
+test_list = [25, 13.34, -43, 105]
+summed = sum(test_list)
+print(summed)
+"""
+"""
+# 3. Write a function to print a list after removing the 0th, 4th and 5th elements.
+def remove_ugly_elements(list_to_modify):
+    del list_to_modify[5]
+    del list_to_modify[4]
+    del list_to_modify[0]
+    print(list_to_modify)
+remove_ugly_elements([12, 13, 14, 15, 16, 17, 18, 19, 20])
+"""
+"""
+# 4. Write a program to shuffle and print 1st element of new list
+from random import shuffle
+test_list = [12, 13, 14, 15, 16, 17, 18, 19, 20]
+shuffle(test_list)
+print(test_list[1])
+"""
